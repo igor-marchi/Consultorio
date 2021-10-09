70fc518b65bcdd3280b4ac8d2aae54b446e901ac
igor-marchi <igor.marchi@havan.com.br>
2021-10-09 17:03:52 -0300

    criada classe e construtor para criação de testes no repositorio de cliente

---
+++ CL.Repository.Tests/Repository/ClienteRepositoryTest.cs
@@ -0,0 +1,33 @@
+﻿using CL.Core.Domain;
+using CL.Data.Context;
+using CL.Data.Repository;
+using CL.FakeData.ClienteData;
+using CL.Manager.Interfaces.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CL.Repository.Tests.Repository
+{
+    public class ClienteRepositoryTest : IDisposable
+    {
+        private readonly IClienteRepository repository;
+        private readonly ClContext context;
+        private readonly Cliente cliente;
+
+        private ClienteFaker clienteFaker;
+
+        public ClienteRepositoryTest()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<ClContext>();
+            optionBuilder.UseInMemoryDatabase("Db_Teste");
+
+            context = new ClContext(optionBuilder.Options);
+            repository = new ClienteRepository(context);
+        }
+
+        public void Dispose()
+        {
+            context.Database.EnsureDeleted();
+        }
+    }
+}
